62d26e180656f4d8349db47c880af4ef8bfbd652
zhoukejun <zhoukejun@126.com>
2019-01-24 16:08:38 +0800

    update

--- Demo/CORTEX_A53_64-bit_BCM2837B0/FreeRTOSConfig.h
+++ Demo/CORTEX_A53_64-bit_BCM2837B0/FreeRTOSConfig.h
@@ -110,7 +110,7 @@ to exclude the API function. */
 #define INCLUDE_vTaskSuspend			1
 #define INCLUDE_vTaskDelayUntil			1
 #define INCLUDE_vTaskDelay			1
-#define INCLUDE_xTaskGetCurrentTaskHandle       0
+#define INCLUDE_xTaskGetCurrentTaskHandle       1
 #define INCLUDE_xTimerPendFunctionCall		0
 #define INCLUDE_eTaskGetState			0
 #define INCLUDE_xTaskAbortDelay			0
